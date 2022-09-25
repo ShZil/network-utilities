e9653c1193847b583414b5ff04d87022187dedef
ShZil <shzil333@gmail.com>
2022-09-25 22:51:18 +0300

    read_ipconfig() created
    Reads a command, decodes output, splits to lines, and returns list[str].

--- Scanner/main.py
+++ Scanner/main.py
@@ -1,7 +1,19 @@
+from subprocess import CalledProcessError, check_output as read_command
+
 __author__ = 'Shaked Dan Zilberman'
 
 # A range for the scanned ports.
-PORTRANGE = range(0, 1024)
+PORT_RANGE = range(0, 1024)
+
+###### IPCONFIG related functions
+def read_ipconfig():
+    """Read the command `>ipconfig /all` from console and decode it to UTF-8 text."""
+    try:
+        return read_command(['ipconfig','/all']).decode(encoding='utf-8', errors='ignore').split('\n')
+    except CalledProcessError:
+        print(">ipconfig /all raised an error.")
+        raise
+
 
 
 def main():
