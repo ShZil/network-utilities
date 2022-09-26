3d84e9c4a785f7509a676f4c8dde59a441a9586e
ShZil <shzil333@gmail.com>
2022-09-26 15:06:11 +0300

    Added util.print_dict

--- Scanner/util.py
+++ Scanner/util.py
@@ -1,5 +1,13 @@
 __author__ = 'Shaked Dan Zilberman'
 
 
+def print_dict(x: dict) -> None:
+    from pygments import highlight, lexers, formatters
+    from json import dumps
+    formatted_json = dumps(x, sort_keys=False, indent=4)
+    colorful_json = highlight(formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter())
+    print(colorful_json)
+
+
 if __name__ == '__main__':
     print("This is a utility module.")
