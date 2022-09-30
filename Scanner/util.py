f107c0c5ca32d3876489fbc8c825fe8332621bb4
ShZil <shzil333@gmail.com>
2022-09-30 16:45:19 +0300

    Added @threadify return list
    defined another internal function `task(function, argument tuple, int index)`, which is called in `Thread(target=)` instead of `f`.
    It executes `f` and saves the return value at the designated index in the list `values`.

--- Scanner/util.py
+++ Scanner/util.py
@@ -3,7 +3,7 @@ from threading import Thread, active_count
 from time import sleep
 
 __author__ = 'Shaked Dan Zilberman'
-MAX_THREADS = 300
+MAX_THREADS: int = 300
 
 
 def print_dict(x: dict) -> None:
@@ -17,7 +17,10 @@ def print_dict(x: dict) -> None:
 def threadify(f):
     """This function turns methods (tasks) into thread-based on-list execution.
     Therefore, the execution will be faster.
-    Return values from the task are ignored.
+    
+    Return values from the task are saved in an array, which is the return value of the decorated function.
+    The values are not messy (which is what normally happens with threading), but organised according to the `args`.
+    Meaning, `returned[0] = f(args[0]); returned[1] = f(args(1))...`.
 
     *This function is blocking.* Its internals run asyncronosly, but calling this will wait until all the tasks are done.
 
@@ -25,6 +28,7 @@ def threadify(f):
     E.g., the function `add(x: int, y: int)`, if it's `@threadify`-ied, will be called by `add([(x0, y0), (x1, y1), (x2, y2)...])`.
     If the function receives a single not-tuple argument, you can just put it in the list `[a, b, c, d]` -> f(a) + f(b) + f(c) + f(d).
     Doesn't support keyword-arguments.
+    The amount of threads per decorated function call is limited by `MAX_THREADS`.
     
     The function can have an `f.options` dict as an attribute, overriding any of these:
     ```
@@ -44,6 +48,9 @@ def threadify(f):
 
     Args:
         f (function): The task to be turned into a threaded task.
+
+    Returns:
+        list: a list of values returned from the multiple calls to the function, sorted by the call order (i.e. not disorganised by the threads).
     """
 
     options={
@@ -57,14 +64,22 @@ def threadify(f):
     except AttributeError:
         pass
 
+
     def wrapper(args: list[tuple]):
         if not isinstance(args, list):
             raise TypeError("Threadify-ied functions must receive a single argument of type list.")
         
-        threads = [Thread(target=f, args=(x if isinstance(x, tuple) else (x, )), daemon=True) for x in args]
+        values = [None] * len(args)
+        
+        def task(func, arg, index):
+            args = arg if isinstance(arg, tuple) else (arg, )
+            value = func(*args)
+            values[index] = value
+        
+        threads = [Thread(target=task, args=(f, x, i), daemon=True) for i, x in enumerate(args)]
         for thread in threads:
             thread.start()
-            while active_count() > MAX_THREADS:
+            while active_count() >= MAX_THREADS:
                 sleep(0.01)
         
         if options["printing"]:
@@ -79,6 +94,8 @@ def threadify(f):
         
         for thread in threads:
             thread.join()
+        
+        return values
         
         
     wrapper.__name__ = f.__name__
