66d78d57d13465932fcae0ebff806d7d5a3aa48f
ShZil <shzil333@gmail.com>
2022-10-30 20:58:33 +0200

    util.handle_module_error docstring and typehints

--- Scanner/util.py
+++ Scanner/util.py
@@ -311,11 +311,55 @@ class InstantPrinting:
         print(self.output.getvalue())
 
 
-def handle_module_error(err):
+def handle_module_error(err: ModuleNotFoundError) -> None:
+    """This function handles a ModuleNotFoundError,
+    attempting to install the not-found module using `pip install`,
+    and restarting the script / instructing the user.
+
+    Code:
+    - necessary imports: sys, os, subprocess
+
+    ```py
     import sys
     from subprocess import check_call as do_command, CalledProcessError
     import os
+    ```
+
+    - print the failure
+
+    ```py
+    print(f"Module `{err.name}` was not found. Attempting `pip install {err.name}`...\n")
+    ```
+
+    - try to pip install it
+
+    ```py
+    try:
+        do_command([sys.executable, "-m", "pip", "install", err.name])
+    ```
+
+    - if failed, request manual installation
+
+    ```py
+    except CalledProcessError:
+        print(f"\\nModule `{err.name}` could not be pip-installed. Please install manually.")
+        sys.exit(1)
+    ```
+
+    - if succeeded, restart the script
     
+    ```py
+    argv = ['\"' + sys.argv[0] + '\"'] + sys.argv[1:]
+    os.execv(sys.executable, ['python'] + argv)
+    ```
+
+    Args:
+        err (ModuleNotFoundError): the error raised and caught.
+    """
+    import sys
+    from subprocess import check_call as do_command, CalledProcessError
+    import os
+
     print(f"Module `{err.name}` was not found. Attempting `pip install {err.name}`...\n")
     try:
         do_command([sys.executable, "-m", "pip", "install", err.name])
