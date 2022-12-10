477fe21712eaa107d8beaa53a973c150306b6995
ShZil <shzil333@gmail.com>
2022-12-11 00:48:28 +0200

    Fix import

--- Scanner/NetworkStorage.py
+++ Scanner/NetworkStorage.py
@@ -188,7 +188,7 @@ class NetworkStorage:
                     entity.merge(special)
             self.data.append(entity)
         
-        from main import hostify_sync, hostify
+        from hostify import hostify_sync, hostify
         hostify_sync([entity.ip for entity in self.waiting.queue if entity.ip != nothing.ip])
         for entity in self.waiting.queue:
             if entity.name == nothing.name:
