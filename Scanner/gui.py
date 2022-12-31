8131561c460b98ea16015a7d66d52dc7704ef582
ShZil <shzil333@gmail.com>
2022-12-31 22:22:08 +0200

    integration with networkx
    I would consider this successful
    although it looks horrible

--- Scanner/gui.py
+++ Scanner/gui.py
@@ -7,7 +7,9 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Color, Ellipse
+from kivy.graphics import Color, Ellipse, Rectangle, Line
+
+import networkx as nx
 
 __author__ = 'Shaked Dan Zilberman'
 
@@ -15,6 +17,34 @@ __author__ = 'Shaked Dan Zilberman'
 def callback(instance):
     print("Hello")
     print(instance)
+
+
+class MyPaintWidget(Widget):
+
+    def on_touch_down(self, touch):
+        update_rect(self, 0)
+
+
+def update_rect(self, value):
+    x, y = self.pos
+    w, h = self.size
+    G = nx.hypercube_graph(5)
+    pos = nx.spiral_layout(G, center=(x + w/2, y + h/2), scale=80)
+    print(pos.values())
+    with self.canvas:
+        Color(0, 1, 0)
+        Rectangle(pos=self.pos, size=self.size)
+        Color(0, 0, 0)
+        
+        for node in G:
+            x0, y0 = pos[node]
+            Ellipse(pos=(x0 - 5, y0 - 5), size=(10, 10))
+        
+        for edge in G.edges:
+            x0, y0 = pos[edge[0]]
+            x1, y1 = pos[edge[1]]
+            Line(points=(x0, y0, x1, y1), width=2)
+    
 
 
 class ButtonColumn(GridLayout):
@@ -41,7 +71,9 @@ class MyApp(App):
             left_menu.add(Button(text='btn' + str(i), font_size=14))
         layout.add_widget(left_menu)
 
-        layout.add_widget(Label(text='hi'))
+        paint = MyPaintWidget()
+        paint.bind(pos=update_rect, size=update_rect)
+        layout.add_widget(paint)
 
         right_menu = ButtonColumn(width=300)
         for i in range(7):
