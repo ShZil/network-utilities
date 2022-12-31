6aade857abbd3dad80d55e9abd9d935fdb42df82
ShZil <shzil333@gmail.com>
2022-12-31 19:59:18 +0200

    GUI with kivy instead
    Canvas works, so if all else fails I can still just manually draw the GUI. Hopefully not, but better than nothing.
    Proof of Concept: I am able to execute python code (`callback`) from the GUI!

--- Scanner/gui.py
+++ Scanner/gui.py
@@ -1,30 +1,39 @@
-from flexx import flx
+import kivy
+kivy.require('2.1.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import Color, Ellipse
 
 __author__ = 'Shaked Dan Zilberman'
 
 
-class Example(flx.Widget):
-
-    def init(self):
-        with flx.VBox():
-            flx.Label(text='Local Network Scanner', style="text-align: center; font-size: 24px; font-weight: bold;")
-
-            with flx.HBox(flex=1):
-                self.hello = flx.Button(text='hello', flex=1)
-                self.bye = flx.Button(text='bye', flex=2)
-    
-
-    @flx.reaction('hello.pointer_click')
-    def _button_clicked(self, *events):
-        ev = events[-1]
-        print("hello")
+class MyPaintWidget(Widget):
+    def on_touch_down(self, touch):
+        print(touch)
+        with self.canvas:
+            Color(0, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
 
 
-def main():
-    app = flx.App(Example)
-    app.launch('app')
-    flx.run()
+def callback(instance):
+    print("Hello")
+    print(instance)
+
+
+class MyApp(App):
+    def build(self):
+        layout = GridLayout(cols=2, row_force_default=True, row_default_height=40)
+        layout.add_widget(MyPaintWidget())
+        btn = Button(text='print out', font_size=14)
+        btn.bind(on_press=callback)
+        layout.add_widget(btn)
+        return layout
 
 
 if __name__ == '__main__':
-    main()
+    MyApp().run()
