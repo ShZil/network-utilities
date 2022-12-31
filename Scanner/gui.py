a5f43da452aa74a00357a8ea9ffda70b113b3d2e
ShZil <shzil333@gmail.com>
2022-12-31 20:56:54 +0200

    4 rectangles layout
    title in centre
    Column 1: buttons
    Column 2: space for the diagram
    Column 3: scans

--- Scanner/gui.py
+++ Scanner/gui.py
@@ -6,6 +6,7 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, Ellipse
 
 __author__ = 'Shaked Dan Zilberman'
@@ -21,29 +22,33 @@ class ButtonColumn(GridLayout):
         super().__init__(cols=1, width=width, size_hint=(None, 1))
         self.buttons = []
     
-    def add_button(self, btn: Button):
+    def add(self, btn: Button):
         super().add_widget(btn)
         self.buttons.append(btn)
 
 
 class MyApp(App):
     def build(self):
+        everything = BoxLayout(orientation='vertical')
+        everything.add_widget(Label(text="Local Network Scanner", size=(0, 70), size_hint=(1, None), font_size=30))
         layout = GridLayout(cols=3)
 
         left_menu = ButtonColumn(width=150)
+        btn = Button(text='print out', font_size=14)
+        btn.bind(on_press=callback)
+        left_menu.add(btn)
         for i in range(5):
-            left_menu.add_button(Button(text='btn' + str(i), font_size=14))
+            left_menu.add(Button(text='btn' + str(i), font_size=14))
         layout.add_widget(left_menu)
 
-        btn = Button(text='print out', font_size=54)
-        btn.bind(on_press=callback)
-        layout.add_widget(btn)
+        layout.add_widget(Label(text='hi'))
 
-        right_menu = ButtonColumn(width=350)
+        right_menu = ButtonColumn(width=300)
         for i in range(7):
-            right_menu.add_button(Button(text='scan' + str(i), font_size=14))
+            right_menu.add(Button(text='scan' + str(i), font_size=14))
         layout.add_widget(right_menu)
-        return layout
+        everything.add_widget(layout)
+        return everything
 
 
 if __name__ == '__main__':
