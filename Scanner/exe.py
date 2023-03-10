278f6716cdfef839147fce044c702db703c6a0b8
ShZil <shzil333@gmail.com>
2023-03-10 17:22:34 +0200

    Pip-install modules if missing

--- Scanner/exe.py
+++ Scanner/exe.py
@@ -1,2 +1,19 @@
 __author__ = 'Shaked Dan Zilberman'
 
+from import_handler import ImportDefence
+from main import *
+from util import *
+from gui import *
+with ImportDefence():
+    import requests
+    import ipaddress
+    import scapy
+    import kivy
+    import networkx
+    import tkinter
+    from threading import Thread
+    import numpy, scipy
+    import win32api
+    import PyQt5
+    import markdown
+
