83494c68df03d66911766726d78279515cea0aaf
ShZil <shzil333@gmail.com>
2023-03-27 18:55:57 +0300

    BUGFIX: Live ARP's ellipsis would disappear fast

--- Scanner/gui.py
+++ Scanner/gui.py
@@ -262,7 +262,7 @@ class Scan:
     
     def finished(self):
         self.is_running = False
-        if self.button.text.endswith('...'):
+        if self.button.text.endswith('...') and 'Live' not in self.button.text:
             self.button.text = self.button.text[:-3]
 
 
