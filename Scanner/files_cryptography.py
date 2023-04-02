dac613ccc62921881b93cd933cb1f44748f524a3
ShZil <shzil333@gmail.com>
2023-04-02 21:58:36 +0300

    Crypto implementation instead

--- Scanner/files_cryptography.py
+++ Scanner/files_cryptography.py
@@ -1,15 +1,42 @@
 from import_handler import ImportDefence
 with ImportDefence():
-    from aes_cipher import DataEncrypter, DataDecrypter
+    import base64
+    import hashlib
+    from Crypto import Random
+    from Crypto.Cipher import AES
+
 
 def password_encrypt(message: bytes, password: str) -> bytes:
-    data_encrypter = DataEncrypter()
-    data_encrypter.Encrypt(message, password)
-    return data_encrypter.GetEncryptedData()
+    cipher = AESCipher(password)
+    return cipher.encrypt(message)
 
 
 def password_decrypt(message: bytes, password: str) -> bytes:
-    data_decrypter = DataDecrypter()
-    data_decrypter.Decrypt(message, password)
-    return data_decrypter.GetDecryptedData()
+    cipher = AESCipher(password)
+    return cipher.decrypt(message)
 
+
+
+class AESCipher:
+    def __init__(self, key):
+        self.bs = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
