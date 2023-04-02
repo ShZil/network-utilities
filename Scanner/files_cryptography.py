ffff77ca0d5b6cc5191ae53b4e6351e4ee2911ed
ShZil <shzil333@gmail.com>
2023-04-02 21:45:52 +0300

    Use aes_cipher instead

--- Scanner/files_cryptography.py
+++ Scanner/files_cryptography.py
@@ -1,43 +1,15 @@
 from import_handler import ImportDefence
 with ImportDefence():
-    import secrets
-    import cryptography
-    from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
+    from aes_cipher import DataEncrypter, DataDecrypter
 
-from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-# Adapted from https://stackoverflow.com/a/55147077
-
-backend = default_backend()
-iterations = 100_000
-
-def _derive_key(password: bytes, salt: bytes, iterations: int = iterations) -> bytes:
-    """Derive a secret key from a given password and salt"""
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(), length=32, salt=salt,
-        iterations=iterations, backend=backend)
-    return b64e(kdf.derive(password))
+def password_encrypt(message: bytes, password: str) -> bytes:
+    data_encrypter = DataEncrypter()
+    data_encrypter.Encrypt(message, password)
+    return data_encrypter.GetEncryptedData()
 
 
-def password_encrypt(message: bytes, password: str, iterations: int = iterations) -> bytes:
-    salt = secrets.token_bytes(16)
-    key = _derive_key(password.encode(), salt, iterations)
-    return b64e(
-        b'%b%b%b' % (
-            salt,
-            iterations.to_bytes(4, 'big'),
-            b64d(Fernet(key).encrypt(message)),
-        )
-    )
-
-
-def password_decrypt(token: bytes, password: str) -> bytes:
-    decoded = b64d(token)
-    salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
-    iterations = int.from_bytes(iter, 'big')
-    key = _derive_key(password.encode(), salt, iterations)
-    return Fernet(key).decrypt(token)
+def password_decrypt(message: bytes, password: str) -> bytes:
+    data_decrypter = DataDecrypter()
+    data_decrypter.Decrypt(message, password)
+    return data_decrypter.GetDecryptedData()
 
