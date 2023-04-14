1c89e9442467f8e2ffbe5b9777bbdc4405bfaa11
ShZil <shzil333@gmail.com>
2023-04-14 13:35:06 +0300

    Final touches

--- Scanner/RecommendProbabilitiesPrimitive.py
+++ Scanner/RecommendProbabilitiesPrimitive.py
@@ -43,6 +43,11 @@ def render_ax1(fig, ax1):
     nodes = nx.draw_networkx_nodes(G, pos, node_size=100, node_color=node_colors, ax=ax1)
     
     colors = [w['weight'] for v, u, w in G.edges(data=True)]
+    cmap = plt.cm.RdYlGn
+    norm = plt.Normalize(vmin=min(colors), vmax=max(colors))
+    sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+    sm.set_array([])
+
     edges = nx.draw_networkx_edges(
         G,
         pos,
@@ -50,16 +55,25 @@ def render_ax1(fig, ax1):
         arrowstyle="->",
         arrowsize=10,
         edge_color=colors,
-        edge_cmap=plt.cm.RdYlGn,
+        edge_cmap=cmap,
+        edge_vmin=min(colors),
+        edge_vmax=max(colors),
         width=2,
         arrows=True,
         ax=ax1
     )
-    labels = nx.draw_networkx_labels(G, pos, ax=ax1)
-    ax1.set_title("Graph")
+
+    plt.colorbar(sm, ax=ax1)
+
+
+    y_off = -0.13
+
+    pos_higher = {k: (v[0], v[1]+y_off) for k, v in pos.items()}
+    labels = nx.draw_networkx_labels(G, pos_higher, ax=ax1)
+    ax1.set_title("Scan's Influence On Each Other")
     
     for node, value in probabilities.items():
-        ax1.annotate(f"{value:.2f}", xy=pos[node], xytext=(-10, -10), textcoords="offset points")
+        ax1.annotate(f"{value:.2f}", xy=pos[node], xytext=(-10, -15), textcoords="offset points")
     
     sm.set_array(node_values)
     plt.colorbar(sm, ax=ax1)
@@ -69,16 +83,24 @@ def render_ax1(fig, ax1):
 def render_ax2(fig, ax2):
     adj_matrix = nx.adjacency_matrix(G)
     adj_array = adj_matrix.toarray()
+        
     im = ax2.imshow(adj_array, cmap='coolwarm', interpolation='nearest', vmin=-1, vmax=1)
-    ax2.set_title("Adjacency Matrix")
+    ax2.set_title("Presented as Adjacency Matrix")
     fig.colorbar(im, ax=ax2)
+
+    node_names = list(G.nodes())
+    ax2.set_xticks(range(len(node_names)))
+    ax2.set_yticks(range(len(node_names)))
+    ax2.set_xticklabels(node_names, rotation=45)
+    ax2.set_yticklabels(node_names)
+    
     for i in range(adj_array.shape[0]):
         for j in range(adj_array.shape[1]):
             ax2.text(j, i, f"{adj_array[i,j]:.2f}", ha="center", va="center", color="black")
 
 
 def render_graph():
-    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 6))
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(17, 7), gridspec_kw={'width_ratios': [1.3, 1]})
     
     render_ax1(fig, ax1)
 
@@ -95,14 +117,14 @@ def step(node):
     for scan, weight in edges:
         probabilities[scan] += weight * p
         print(f"Changed {scan} by {weight * p}")
+    normalise()
     
 
 def main():
     construct_graph()
     normalise()
     render_graph()
-    step("ARP Live")
-    normalise()
+    step("ICMP Sweep")
     render_graph()
 
 
