9e8e3a646f2301e943af3aa61be1b39d71adfa06
ShZil <shzil333@gmail.com>
2023-05-03 00:38:00 +0300

    Honestly just trying whatever with this crash

--- Scanner/gui/Screens/KnowScreen.py
+++ Scanner/gui/Screens/KnowScreen.py
@@ -104,46 +104,50 @@ class KnowScreenInfoLabel(ScrollView):
 class KnowScreenDeviceProfileButton(Button):
     def __init__(self, **kwargs):
         super().__init__(text="Device Profile", size_hint=(.2, .1), pos_hint={'x': 0.39, 'top': 0.1}, font_size=20, background_color=[0, 1, 0, 1], font_name="Roboto", **kwargs)
-        self.bind(on_press=self.device_profile)
+        self.bind(on_press=device_profile)
         Hover.add(self)
 
-    def device_profile(*_):
-        from NetworkStorage import NetworkStorage, match, SpecialInformation
+def device_profile(*a):
+    print(a)
 
-        def _match_device(address):
-            try:
-                entity = match(address)
-                for item in NetworkStorage():
-                    if entity.equals(item):
-                        return item
-            except ValueError:
-                name = address
-                if name == "Unknown":
-                    return None
-                for item in NetworkStorage():
-                    if item.name == name:
-                        return item
-            return None
+    def _match_device(address):
+        from NetworkStorage import NetworkStorage, match
+        try:
+            entity = match(address)
+            for item in NetworkStorage():
+                if entity.equals(item):
+                    return item
+        except ValueError:
+            name = address
+            if name == "Unknown":
+                return None
+            for item in NetworkStorage():
+                if item.name == name:
+                    return item
+        return None
 
-        def _construct_content(info: dict):
-            return '\n\n'.join([f"### {key}:\n{value}" for key, value in info.items()])
+    def _construct_content(info: dict):
+        return '\n\n'.join([f"### {key}:\n{value}" for key, value in info.items()])
 
-        def _device_profile():
-            address = get_string("Device Profile - Choose Address", "Insert device's MAC/IP/IPv6 address or name:")
-            entity = _match_device(address)
-            if entity is None:
-                popup("Device Profile", "The device was not found.\nCheck whether you wrote the address correctly.", warning=True)
-                return
-            regular_info = entity.to_dict()
-            special_info = SpecialInformation()[entity]
-            information = {**regular_info, **special_info}
-            try:
-                popup("Device Profile", _construct_content(information), info=True)
-            except Exception as e:
-                print(e)
-            print("Popped")
+    def _device_profile():
+        from NetworkStorage import SpecialInformation
+        address = get_string("Device Profile - Choose Address", "Insert device's MAC/IP/IPv6 address or name:")
+        entity = _match_device(address)
+        if entity is None:
+            popup("Device Profile", f"The device was not found.\nCheck whether you wrote the address correctly.\nThe address: `{address}`", warning=True)
+            return
+        regular_info = entity.to_dict()
+        special_info = SpecialInformation()[entity]
+        information = {**regular_info, **special_info}
+        print(_construct_content(information))
+        # try:
+        #     popup("Device Profile", _construct_content(information), info=True)
+        # except Exception as e:
+        #     print(e)
+        print("Popped")
 
-        Thread(target=_device_profile).start()
+    Thread(target=_device_profile).start()
+    # _device_profile()
 
 
 if __name__ == '__main__':
