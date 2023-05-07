bc9f6dfc28241b996c0b29b6f284c11796fe78e4
ShZil <shzil333@gmail.com>
2023-05-07 22:33:36 +0300

    Update database and PATH

--- Scanner/db.py
+++ Scanner/db.py
@@ -16,6 +16,7 @@ CREATE_INFORMATION_TABLE = """CREATE TABLE IF NOT EXISTS `information` (
 INSERT_INFORMATION = "INSERT INTO information VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
 GET_ALL_SCANS = "SELECT name FROM information"
 SELECT_SPECIFIC_SCAN = "SELECT * FROM information WHERE name=?"
+PATH = "scans.db"
 INFORMATION_DATA = [
     # name: str, description: longstr, time: float, reward: str, certainty: int, safety: int, mode: int, repeats: bool
     # NOTE: `mode` is reserved for future use.
@@ -26,16 +27,17 @@ INFORMATION_DATA = [
     ("Live ARP", "**Step 1:** executes `arp -a` to see which addresses are already saved on the device's lookup table.\n**Step 2:** Listens indefinitely to who-has/is-at packets, using the ARP protocol.\nCompletely silent -- sends no packets, just reads those that are received. <Ether> <ARP op=1 (who-has) or op=2 (is-at)>", 0, "MAC & IP addresses of active devices", 99, 100, 1, False),
     ("OS-ID", "Identifies the Operating System (OS) of all known devices. This uses techniques called TCP/IP stack fingerprinting, and inspects the IP:TTL and TCP:window. This is very close to guessing, so be doubtful of the results.\nIt sends no packets by itself (i.e. it's safe),\nbut looks at the packets received from other (possibly unsafe) scans, or any packets that were sniffed silently.", 0, "Probable Operating System of devices", 30, 100, 1, False),
     ("TCP Ports", "Transmission Control Protocol (TCP) Half-Open Port Scan. A usual TCP 3-way handshake is (SYN) - (SYN+ACK) - (ACK); this scan sends a SYN packet, and waits for a SYN+ACK packet, but never completes the handshake. <IP dst=specific_ip> <TCP SYN>", 33, "Open/Reset/Closed TCP ports on a device", 90, 80, 1, True),
-    ("UDP Ports", "User Datagram Protocol (UDP) Scanning, can detect some closed ports, because they will respond with an ICMP Port Unreachable error packet. <IP dst=specific_ip> <UDP> -> <IP> <ICMP type=3 (unreachable)>", 0.1, "", 0, 0, 0, True),
-    ("woo!", "", 0.1, "", 0, 0, 0, True),
+    ("UDP Ports", "User Datagram Protocol (UDP) Scanning, can detect some closed ports, because they will respond with an ICMP Port Unreachable error packet. <IP dst=specific_ip> <UDP> -> <IP> <ICMP type=3 (unreachable)>", 0.1, "", 0, 0, 1, True),
+    ("woo!", "", 0.1, "", 0, 0, 1, True),
     ("Public Address", "Gets the outside IP address of the router, using `https://api.ipify.org`. <IP> <TCP> <HTTPS GET / HTTP/2 [Host: api.ipify.org]>", 1.3, "Public IP address of the router", 97, 90, 1, True),
     ("Traceroute", "Using ICMP and IP:ttl, find the route to a distant device.", 0.1, "IP addresses of all devices in the route", 0, 0, 1, True),
-    ("Log Packets", "Log all the packets that were silently sniffed.", 0, "The packets are already saved, you're just viewing them.", 99, 100, 1, True)
+    ("Log Packets", "Log all the packets that were silently sniffed.", 0, "The packets are already saved, you're just viewing them.", 99, 100, 0, True),
+    ("Device Profile", "Log all the packets that were silently sniffed.", 0, "The packets are already saved, you're just viewing them.", 99, 100, 0, True)
 ]
 
 
 def get_information_about_scan(name: str):
-    connection = sqlite3.connect('scans.db')
+    connection = sqlite3.connect(PATH)
     cursor = connection.cursor()
     try:
         cursor.execute(SELECT_SPECIFIC_SCAN, (name, ))
@@ -47,7 +49,7 @@ def get_information_about_scan(name: str):
 
 
 def get_scans():
-    connection = sqlite3.connect('scans.db')
+    connection = sqlite3.connect(PATH)
     cursor = connection.cursor()
     try:
         cursor.execute(GET_ALL_SCANS)
