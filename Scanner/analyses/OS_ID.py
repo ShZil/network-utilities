87dabd79e38c599aaae51baaf25f31ed782cbd35
ShZil <shzil333@gmail.com>
2023-05-16 00:11:37 +0300

    Use OS-ID as an Observer

--- Scanner/analyses/OS_ID.py
+++ Scanner/analyses/OS_ID.py
@@ -5,6 +5,7 @@ from threading import Thread
 def operating_system_fingerprinting() -> None:
     """This function does the OS-ID scan-like action.
     It's an infinite action, so this method starts a thread to run that action.
+    (Updated: this method creates an observer of PacketSniffer)
     For further explanation, execute this file alone, or read OS-ID's information.
     """
     from NetworkStorage import here, SpecialInformation, NetworkEntity, nothing
@@ -15,23 +16,20 @@ def operating_system_fingerprinting() -> None:
         # do more testing on this *************
         return "Linux or Android" if packet.ttl <= 64 else "Windows"
 
-    def _fingerprinter():
-        from globalstuff import terminator
-        while not terminator.is_set():
-            # for each packet,
-            for packet in PacketSniffer():
-                # if it originates at some other computer...
-                if packet.src == here.ip:
-                    continue
-                entity = NetworkEntity(mac=packet[Ether].src, ip=packet[IP].src, ipv6=nothing.ipv6, name="Unknown")
-                # ...that has not yet been OS-ID'd
-                if entity in SpecialInformation():
-                    continue
-                # Then guess what it's OS is, using `_determine_os`, and save it to `SpecialInformation`.
-                SpecialInformation()[entity, 'OS'] = _determine_os(packet)
-            sleep(10)
+    def fingerprint(packet):
+        # for each packet,
+        # if it originates at some other computer...
+        if packet.src == here.ip:
+            return
+        entity = NetworkEntity(mac=packet[Ether].src, ip=packet[IP].src, ipv6=nothing.ipv6, name="Unknown")
+        # ...that has not yet been OS-ID'd
+        if entity in SpecialInformation()['OS']:
+            return
+        # Then guess what it's OS is, using `_determine_os`, and save it to `SpecialInformation`.
+        SpecialInformation()[entity, 'OS'] = _determine_os(packet)
+    
+    PacketSniffer.add_observer(fingerprint)
 
-    Thread(target=_fingerprinter).start()
 
 
 if __name__ == '__main__':
