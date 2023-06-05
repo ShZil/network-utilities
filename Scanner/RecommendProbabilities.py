eef591a1db77f73cb13c98d07ca4fb3dde61cb0a
ShZil <shzil333@gmail.com>
2023-06-06 00:46:55 +0300

    rename G to R

--- Scanner/RecommendProbabilities.py
+++ Scanner/RecommendProbabilities.py
@@ -4,7 +4,7 @@ import matplotlib.colors as mcolors
 import matplotlib.cm as cm
 import random
 
-G = nx.DiGraph()
+R = nx.DiGraph()
 probabilities = {}
 
 
@@ -17,9 +17,9 @@ def construct_graph():
     :return: A graph with nodes and edges
     """
     
-    global probabilities, G
-    G.add_nodes_from(["ARP Sweep", "ICMP Sweep", "ARP Live", "ICMP Live", "TCP Ports", "Public Address", "Trace Route", "Reveal Myself"])
-    G.add_weighted_edges_from([
+    global probabilities, R
+    R.add_nodes_from(["ARP Sweep", "ICMP Sweep", "ARP Live", "ICMP Live", "TCP Ports", "Public Address", "Trace Route", "Reveal Myself"])
+    R.add_weighted_edges_from([
         # (v, u, w: float)
         ("ARP Sweep", "ARP Live", 0.4),
         ("ICMP Sweep", "ICMP Live", 0.4),
@@ -32,10 +32,10 @@ def construct_graph():
         ("ICMP Live", "TCP Ports", 0.2),
         ("Trace Route", "TCP Ports", 0.5)
     ])
-    G.add_weighted_edges_from(list((n, n, -0.5) for n in G.nodes))
+    R.add_weighted_edges_from(list((n, n, -0.5) for n in R.nodes))
     # positive values are "Yeah, if you executed `v`, consider executing `u`".
     # negative values are "If you executed `v` please do not execute `u`".
-    probabilities = {node: 1 for node in G}
+    probabilities = {node: 1 for node in R}
 
 
 def normalise():
@@ -53,7 +53,7 @@ def normalise():
 def render_ax1(fig, ax1):
     """
     The render_ax1 function takes a figure and an axis as arguments.
-    It then uses the networkx library to draw the graph G on that axis,
+    It then uses the networkx library to draw the graph R on that axis,
     using a circular layout. The node colors are determined by their probabilities,
     and the edge colors are determined by their weights.
     
@@ -61,7 +61,7 @@ def render_ax1(fig, ax1):
     :param ax1: Specify which axis to draw the graph on
     """
     
-    pos = nx.circular_layout(G)
+    pos = nx.circular_layout(R)
     node_values = list(probabilities.values())
 
     # Node colormap and colorbar
@@ -70,17 +70,17 @@ def render_ax1(fig, ax1):
     sm_nodes = cm.ScalarMappable(norm=norm_nodes, cmap=cmap_nodes)
 
     node_colors = [sm_nodes.to_rgba(value) for value in node_values]
-    nodes = nx.draw_networkx_nodes(G, pos, node_size=100, node_color=node_colors, ax=ax1)
+    nodes = nx.draw_networkx_nodes(R, pos, node_size=100, node_color=node_colors, ax=ax1)
 
     # Edge colormap and colorbar
-    colors = [w['weight'] for v, u, w in G.edges(data=True)]
+    colors = [w['weight'] for v, u, w in R.edges(data=True)]
     cmap_edges = plt.cm.coolwarm_r
     norm_edges = plt.Normalize(vmin=-1, vmax=1)
     sm_edges = plt.cm.ScalarMappable(norm=norm_edges, cmap=cmap_edges)
     sm_edges.set_array([])
 
     edges = nx.draw_networkx_edges(
-        G,
+        R,
         pos,
         node_size=100,
         arrowstyle="->",
@@ -101,7 +101,7 @@ def render_ax1(fig, ax1):
     y_off = -0.13
 
     pos_higher = {k: (v[0], v[1] + y_off) for k, v in pos.items()}
-    labels = nx.draw_networkx_labels(G, pos_higher, ax=ax1)
+    labels = nx.draw_networkx_labels(R, pos_higher, ax=ax1)
     ax1.set_title("Scan's Influence On Each Other")
 
     for node, value in probabilities.items():
@@ -111,21 +111,21 @@ def render_ax1(fig, ax1):
 def render_ax2(fig, ax2):
     """
     The render_ax2 function takes a figure and an axis as arguments.
-    It then creates an adjacency matrix from the graph G, which is defined in the global scope.
+    It then creates an adjacency matrix from the graph R, which is defined in the global scope.
     The adjacency matrix is converted to a numpy array, and then plotted using imshow().
     The title of the plot is set to &quot;Presented as Adjacency Matrix&quot;.  The colorbar() function was commented out because it was not working properly for me (it would only show up if I called plt.show() after calling render_ax2(), but that would also cause both plots to be displayed at once).  The node names are extracted
     
     :param fig: Pass in the figure object that we created earlier
     :param ax2: Specify the axis that we want to draw on
     """
-    adj_matrix = nx.adjacency_matrix(G)
+    adj_matrix = nx.adjacency_matrix(R)
     adj_array = adj_matrix.toarray()
 
     im = ax2.imshow(adj_array, cmap='coolwarm_r', interpolation='nearest', vmin=-1, vmax=1)
     ax2.set_title("Presented as Adjacency Matrix")
     # fig.colorbar(im, ax=ax2)
 
-    node_names = list(G.nodes())
+    node_names = list(R.nodes())
     ax2.set_xticks(range(len(node_names)))
     ax2.set_yticks(range(len(node_names)))
     ax2.set_xticklabels(node_names, rotation=45)
@@ -160,7 +160,7 @@ def step(node):
     
     :param node: Specify which node to start from
     """
-    edges = G.edges(node, data=True)
+    edges = R.edges(node, data=True)
     edges = [(dst, data['weight']) for src, dst, data in edges]
     # print(edges)
     p = probabilities[node]
@@ -172,13 +172,13 @@ def step(node):
     maxp = 0.99
     reduction_factor = 0.9
     if prev is not None:
-        if G.has_edge(prev, node):
-            # print(G[prev][node])
-            G[prev][node]['weight'] += dp * random.uniform(0.8, 1.2)
-            if G[prev][node]['weight'] > maxp:
-                G[prev][node]['weight'] *= reduction_factor
+        if R.has_edge(prev, node):
+            # print(R[prev][node])
+            R[prev][node]['weight'] += dp * random.uniform(0.8, 1.2)
+            if R[prev][node]['weight'] > maxp:
+                R[prev][node]['weight'] *= reduction_factor
         else:
-            G.add_edge(prev, node, weight=random.uniform(dp/2, dp))
+            R.add_edge(prev, node, weight=random.uniform(dp/2, dp))
     prev = node
     normalise()
 
